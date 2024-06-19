3d5e528e4219d63865c11a892cf7cb1ef55bea54
Kwansiri <6629011019@cdti.ac.th>
2024-06-19 15:37:47 +0700

    update

---
+++ lab3-2.py
@@ -0,0 +1,4 @@
+l = int(input("รับค่า"))
+while l>=0:
+    print(l**2)
+    l = int(input("รับค่า"))
---
+++ lab3-3.py
@@ -0,0 +1,15 @@
+for s in 1,2,3,4,5:
+    print(s)
+for s in 'one','two','three':
+    print(s)
+for letter in 'python' :
+    print (letter)
+    
+print("\n")
+fruits =['banana','apple','mango']
+for word in fruits :
+    print(word)
+    
+a=['cat','window','defenestrate']
+for x in a:
+    print (x)
